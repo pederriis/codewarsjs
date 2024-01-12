b5b9364bd1c85d5e918197f65e1d8f8b3d001cc3
Peder Riis <pede0821@koldinghfogvuc.dk>
2024-01-12 13:13:59 +0100

    DeleteOccurences done

---
+++ DeleteOccurences.js
@@ -0,0 +1,25 @@
+console.log(deleteNth([1,1,3,3,7,2,2,2,2], 3));
+
+function deleteNth(arr,n){
+   
+    const counts = {};
+    let output=[];
+
+for (let num of arr) {
+ 
+if (counts[num]==null){
+counts[num]=1;
+output.push(num);
+
+}
+  else if (counts[num]<n){
+    output.push(num);
+
+    counts[num]++;
+}
+
+
+}
+
+return output;
+  }
